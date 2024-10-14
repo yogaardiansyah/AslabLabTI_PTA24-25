145fb1d929424cf67193cd975305cdf5c7b2dd7f
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2024-10-14 19:30:43 +0700

    feat:Upload LA
    Upload LA Pengenalan Python

---
+++ Pertemuan 1 - Pengenalan Python/Laporan Akhir/la.py
@@ -0,0 +1,29 @@
+nama = "a" #isi dengan nama kalian
+kelas = "a" #isi dengan kelas kalian
+npm = "a" #isi dengan kelas kalian
+
+print("==================================================")
+print("Halo perkenalkan nama saya ",nama,"\ndari kelas ",kelas,"\nmemiliki npm ",npm)
+print("Hari ini saya telah belajar tentang print(), operator, dan variabel serta tipe data sederhana")
+
+print("\nberikut contoh program menghitung luas segitiga dan lingkaran\n")
+print("==================================================")
+alas = 5
+tinggi = 6
+
+luas_segitiga = alas * tinggi //2
+
+print("berikut adalah hasil perhitungan luas segitiga dengan alas 5 dan tinggi 6 :",luas_segitiga)
+
+r = 7
+
+luas_lingkaran = r * r * 3.14
+print("\nberikut adalah adalah hasil perhitungan luas lingkaran dengan jari jari 7 :",luas_lingkaran)
+
+print("==================================================")
+print("\nberikut ini tipe data dari perhitungan luas lingkaran dan segitiga")
+print(type(luas_lingkaran))
+print(type(luas_segitiga))
+print("==================================================")
+
+#Kode dijelaskan bagaimana cara kerja dari perintah perintah diatas(tidak perlu perbaris dijabarkan)
